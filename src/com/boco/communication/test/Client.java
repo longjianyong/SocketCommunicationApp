47cc94433695a0be310701c559976231275e9b01
longjianyong <329916948@qq.com>
2015-04-13 16:37:09 +0800

    test description

--- src/com/boco/communication/test/Client.java
+++ src/com/boco/communication/test/Client.java
@@ -7,6 +7,7 @@ import com.boco.communication.socket.bean.SessionInfo;
 public class Client {
 
 	public static void main(String[] args) {
+//		git分布式项目代码管理
 		SessionInfo sessionI= new SessionInfo("127.0.0.1", 8999);
 		sessionI.setSessionType(2);
 		RequestHandlerUtil.send(sessionI, AdamEnum.ADAM_HEAD_TYPE_ENUM.WEB_2_PROBER_REQUEST_VALUE, "ddd".getBytes());
